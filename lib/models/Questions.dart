eb22ea5c82810a2159f0abbb371656a4a55f1b24
lucas521241 <lucasschneider19@gmail.com>
2022-09-23 20:54:44 -0300

    first commit

---
+++ lib/models/Questions.dart
@@ -0,0 +1,35 @@
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({this.id, this.question, this.answer, this.options});
+}
+
+const List sample_data = [
+  {
+    "id": 1,
+    "question":
+        "Flutter é um kit de desenvolvimento de software de interface do usuário de código aberto criado por ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "Quando o google liberou o Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "Um local de memória que contém uma única letra ou número.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answer_index": 2,
+  },
+  {
+    "id": 4,
+    "question": "Qual comando você usa para enviar dados para a tela?",
+    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "answer_index": 2,
+  },
+];
